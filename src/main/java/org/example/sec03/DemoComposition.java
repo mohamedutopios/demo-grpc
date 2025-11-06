72da231a9d92749b80facab8b712c6fd1c726695
mohamed <mohamed@utopios.net>
2025-11-06 16:26:39 +0100

    add sec03

--- src/main/java/org/example/sec03/DemoComposition.java
+++ src/main/java/org/example/sec03/DemoComposition.java
@@ -1,4 +1,42 @@
 package org.example.sec03;
 
+import org.example.models.sec03.Address;
+import org.example.models.sec03.School;
+import org.example.models.sec03.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DemoComposition {
+
+
+    private static final Logger log = LoggerFactory.getLogger(DemoComposition.class);
+
+
+    public static void main(String[] args) {
+
+        var address = Address.newBuilder()
+                .setStreet("123 main st")
+                .setCity("atlanta")
+                .setState("GA")
+                .build();
+        var student = Student.newBuilder()
+                .setName("sam")
+                .setAddress(address)
+                .build();
+
+        var school = School.newBuilder()
+                .setId(1)
+                .setName("high school")
+                .setAddress(address.toBuilder().setStreet("234 main st").build())
+                .build();
+
+        log.info("school: {}", school);
+
+        log.info("student: {}", student);
+
+
+    }
+
+
+
 }
--- src/main/java/org/example/sec03/DemoPerformance.java
+++ src/main/java/org/example/sec03/DemoPerformance.java
@@ -1,4 +1,68 @@
 package org.example.sec03;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.example.models.sec03.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DemoPerformance {
+
+    private static final Logger log = LoggerFactory.getLogger(DemoPerformance.class);
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+
+    public static void main(String[] args) {
+
+        var protoPerson = Person.newBuilder()
+                .setLastName("sam")
+                .setAge(12)
+                .setEmail("sam@gmail.com")
+                .setEmployed(true)
+                .setSalary(1000.2345)
+                .setBankAccountNumber(123456789012L)
+                .setBalance(-10000)
+                .build();
+
+        var jsonPerson = new JsonPerson("sam", 12, "sam@gmail.com", true, 1000.2345, 123456789012L, -10000);
+       // json(jsonPerson);
+       // proto(protoPerson);
+                for (int i = 0; i < 1; i++) {
+            runTest("json", () -> json(jsonPerson));
+            runTest("proto", () -> proto(protoPerson));
+        }
+
+    }
+
+    private static void proto(Person person){
+        try {
+            var bytes = person.toByteArray();
+            log.info("proto bytes length: {}", bytes.length);
+            Person.parseFrom(bytes);
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void json(JsonPerson person){
+        try{
+            var bytes = mapper.writeValueAsBytes(person);
+            log.info("json bytes length: {}", bytes.length);
+            mapper.readValue(bytes, JsonPerson.class);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void runTest(String testName, Runnable runnable){
+        var start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            runnable.run();
+        }
+        var end = System.currentTimeMillis();
+        log.info("time taken for {} - {} ms", testName, (end - start));
+    }
+
+
 }
